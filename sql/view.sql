309c5ea9b602dd9def491d84fff85743c253df8f
Pedro Neris <pedrolucaspn13@gmail.com>
2025-07-07 14:51:11 -0300

    changing view

--- sql/view.sql
+++ sql/view.sql
@@ -1,8 +1,11 @@
 CREATE VIEW media_prato AS
-SELECT prato.id, prato.nome, prato.icone, count (avaliacao.id) AS qtd_avaliacoes, AVG (avaliacao.nota) AS media_avaliacoes
+SELECT prato.id, prato.nome, prato.icone, count (DISTINCT avaliacao.id) AS qtd_avaliacoes, AVG (avaliacao.nota) AS media_avaliacoes,
+    count(DISTINCT cardapio_prato.id_cardapio) AS qtd_cardapios
 FROM prato
 LEFT JOIN
 avaliacao  ON
 avaliacao.id_prato=prato.id
+LEFT JOIN cardapio_prato ON 
+cardapio_prato.id_prato = prato.id
 GROUP BY prato.id, prato.nome, prato.icone
-ORDER BY media_avaliacoes DESC;
+ORDER BY media_avaliacoes DESC NULLS LAST, qtd_avaliacoes DESC;
