e41cfbbac85f79abd00a9fea7d5b0ea9befa4815
gabrielpr208 <gabriel.rodrigues.gpr@gmail.com>
2025-07-07 11:32:58 -0300

    adding validation to procedure

--- sql/procedure.sql
+++ sql/procedure.sql
@@ -10,8 +10,47 @@ CREATE PROCEDURE inserir_avaliacao(
 LANGUAGE plpgsql
 AS $$
 DECLARE
+    usuario_existe INTEGER;
+    prato_existe INTEGER;
     avaliacao_existe INTEGER;
 BEGIN
+    SELECT COUNT(*) INTO usuario_existe
+    FROM Usuario
+    WHERE id = new_aval_id_usuario;
+
+    IF usuario_existe = 0 THEN
+        RAISE EXCEPTION 'Usuário não existe.', new_aval_id_usuario;
+    END IF;
+
+
+    SELECT COUNT(*) INTO prato_existe
+    FROM Prato
+    WHERE id = new_aval_id_prato;
+
+    IF prato_existe = 0 THEN
+        RAISE EXCEPTION 'Prato não existe.', new_aval_id_prato;
+    END IF;
+
+    
+    IF new_aval_data_consumo > CURRENT_DATE THEN
+        RAISE EXCEPTION 'A data de consumo não pode ser futura.';
+    END IF;
+
+    IF new_aval_data_avaliacao > CURRENT_DATE THEN
+        RAISE EXCEPTION 'A data da avaliação não pode ser futura.';
+    END IF;
+
+
+    IF new_aval_tipo_refeicao NOT IN ('Almoço', 'Café da manhã', 'Janta') THEN
+        RAISE EXCEPTION 'Tipo de refeição inválido. Deve ser Almoço, Café da manhã ou Janta.', new_aval_tipo_refeicao;
+    END IF;
+
+
+    IF new_aval_nota_avaliacao < 0 OR new_aval_nota_avaliacao > 5 THEN
+        RAISE EXCEPTION 'Nota inválida. Deve estar entre 0 e 5.', new_aval_nota_avaliacao;
+    END IF;
+
+
     SELECT COUNT(*) INTO avaliacao_existe
     FROM Avaliacao
     WHERE id_usuario = new_aval_id_usuario
@@ -43,3 +82,4 @@ BEGIN
         RAISE NOTICE 'Este prato já foi avaliado para esta refeição nesta data.';
     END IF;
 END;
+$$;
